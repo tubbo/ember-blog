17ef0c1f97a7499c006c2089df576b1ac69c0ee4
Tom Scott <tscott@telvue.com>
2015-01-23 01:46:49 -0500

    Document Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,16 @@
+/**
+ * Simple S3 build script for pushing files to Amazon. Takes all files
+ * in ./dist and uploads them to a configured S3 bucket. Use Amazon's
+ * web interface to configure that bucket for public display, and you
+ * got yourself a web site! Bonus points if you use CloudFront to
+ * distribute these assets all over the world for ultra-fast render
+ * times.
+ */
+
 var grunt = require('grunt'),
     fs = require('fs'),
     path = require('path'),
-    templates = fs.readdirSync(path.join(root.path, 'public/articles'));
+    files = fs.readdirSync(path.join(root.path, 'dist'));
 
 grunt.loadNpmTasks('grunt-s3');
 
@@ -16,7 +25,7 @@ grunt.initConfig({
         "Cache-Control": "max-age=630720000, public",
         "Expires": new Date(Date.now() + 63072000000).toUTCString()
       },
-      sync: templates.map(function(file) {
+      sync: files.map(function(file) {
         return {
           src: file,
           dest: path.basename(file),
