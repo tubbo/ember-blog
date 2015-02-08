36a4640b785c3e942b681f93548ae9c47ca84edd
Tom Scott <tscott@telvue.com>
2015-02-08 17:44:15 -0500

    Remove unused code

--- app/models/page.js
+++
@@ -1,7 +0,0 @@
-import DS from 'ember-data';
-import Ember from 'ember';
-
-export default DS.Model.extend({
-  title: DS.attr('string'),
-  body: new Ember.Handlebars.SafeString('<span class="loading">Loading Content...</span>')
-});
