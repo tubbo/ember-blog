9c3a4a92e6f7043990776b332c3b85fc67c22724
Tom Scott <tscott@weblinc.com>
2015-06-20 20:43:42 -0400

    Add footer copyright

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,15 +3,5 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return this.store.find('article');
-  },
-
-  afterModel: function(model) {
-    this.set('title', 'articles');
-    model.forEach(function(article) {
-      var url = '/articles/'+article.get('id')+'.preview.html';
-      Ember.$.get(url, function(html) {
-        article.set('body', new Ember.Handlebars.SafeString(html));
-      });
-    });
   }
 });
