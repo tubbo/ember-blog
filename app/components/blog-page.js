99ae7981fcd91624bc3f0724dc60f8f06727742b
Tom Scott <tscott@weblinc.com>
2015-06-25 21:01:31 -0400

    Fix page display

--- app/components/blog-page.js
+++ app/components/blog-page.js
@@ -1,4 +1,15 @@
+/* global marked */
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: 'article',
+  classNames: ['page'],
+
+  html: function() {
+    return new Ember.Handlebars.SafeString(marked(this.get('body')));
+  }.property('body'),
+
+  lastUpdated: function() {
+    return 'today';
+  }
 });
